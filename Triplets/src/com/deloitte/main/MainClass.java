b669cf5fea40de717fa7bba24e5a06ef9685862a
madhu12 <madhu12@192.168.1.31>
2018-08-02 16:54:29 +0530

    Assessment

--- Triplets/src/com/deloitte/main/MainClass.java
+++ Triplets/src/com/deloitte/main/MainClass.java
@@ -1,36 +1,31 @@
 package com.deloitte.main;
 
+import java.util.Scanner;
 
-	import java.util.Scanner;
+public class MainClass {
 
-	public class MainClass {
+	public static void main(String[] args) {
 
-	 public static void main(String[] args) {
-	  // TODO Auto-generated method stub
-	  
-	  Scanner sc = new Scanner(System.in);
-	  System.out.println("Enter size of array");
-	  int n = sc.nextInt();
-	  int array[] = new int[n];
-	  if (n>2) {
-	   
-	   System.out.print("Enter the elements of the array");
-	   for(int i=0;i<n;i++)
-	   {
-	    array[i]=sc.nextInt();
-	   }
-	   boolean x= Triplets.searchTrip(array);  
-	    if(x)
-	     System.out.println("TRUE");
-	    else
-	     System.out.println("FALSE");
-	  }
-	  else if (n<3)
-	  {
-	   System.out.println("Please enter a size greater than or equal to 3");
-	  }
-	  sc.close();
-	 }
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter array size");
+		int n = sc.nextInt();
+		int array[] = new int[n];
+		if (n > 2) {
 
+			System.out.print("Enter the elements of the array");
+			for (int i = 0; i < n; i++) {
+				array[i] = sc.nextInt();
+			}
+			boolean status = Triplets.searchTrip(array);
+			if (status)
+				System.out.println("TRUE");
+			else
+				System.out.println("FALSE");
+		} else if (n < 3) {
+			System.out.println("Note: enter a size greater than or equal to 3");
+		}
+		sc.close();
 	}
+
+}
 
